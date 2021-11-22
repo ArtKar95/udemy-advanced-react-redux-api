718c9decc22cbb63aad2d5868c4671515e165a6e
ArtKar95 <artkarapetyan1995@gmail.com>
2021-11-22 22:36:35 +0400

    fix cors errors

--- index.js
+++ index.js
@@ -1,22 +1,23 @@
-import dotenv from "dotenv";
-const envPath = ".env";
+import dotenv from 'dotenv';
+const envPath = '.env';
 dotenv.config({ path: envPath });
-import express from "express";
-import http from "http";
-import bodyParser from "body-parser";
-import morgan from "morgan";
-import route from "./route.js";
-import mongoose from "mongoose";
-import User from "./models/user.js";
-
+import express from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import morgan from 'morgan';
+import route from './route.js';
+import mongoose from 'mongoose';
+import User from './models/user.js';
+import cors from 'cors';
 const app = express();
 
 //DB setup
 mongoose.connect(process.env.MONGO_API);
 
 //App setup
-app.use(morgan("combined"));
-app.use(bodyParser.json({ type: "*/*" }));
+app.use(morgan('combined'));
+app.use(cors());
+app.use(bodyParser.json({ type: '*/*' }));
 route(app);
 //Server setup
 const port = process.env.PORT || 3090;
